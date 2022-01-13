69cbebec61143870b7c4535dc4c2d52ef47338f4
scollo26 <giovanniscollo26@gmail.com>
2022-01-13 13:18:44 +0100

    init

---
+++ src/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+      <h1>{{title}}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return{
+            title: "lorem"
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
